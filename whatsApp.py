610a6453063468859d66c37b105e791ec9720414
rainerceribelli <rainer.rustiguel2013@gmail.com>
2023-05-05 16:40:21 -0300

    Alteração no menu
    Remoção do menu antigo, para a nova tela do menu

--- whatsApp.py
+++ whatsApp.py
@@ -6,22 +6,22 @@ import menu
 def main():
     try:
         #arquivo = r'CSV_AET001_AET002.csv'
-        #IdOSWhats = 1116260
-        #IdOSEletronico = 1115285
-        #agendaEnvio = "N"
-        #qtdeMinutos = "N"
+        IdOSWhats = 1116260
+        IdOSEletronico = 1115285
+        agendaEnvio = "N"
+        qtdeMinutos = "N"
 
         arquivo = sys.argv[1]
-        IdOSWhats = sys.argv[2]
-        IdOSEletronico = sys.argv[3]
-        agendaEnvio = sys.argv[6]
-        qtdeMinutos = sys.argv[7]
+        #IdOSWhats = sys.argv[2]
+        #IdOSEletronico = sys.argv[3]
+        #agendaEnvio = sys.argv[6]
+        #qtdeMinutos = sys.argv[7]
 
         processamento.make_json(arquivo)
         processamento.arquivoJSON(IdOSWhats, IdOSEletronico, agendaEnvio, qtdeMinutos)
     
     except IndexError:
-        menu.menu_inicial()
+        menu.menu()
 
 if __name__ == '__main__':
     main()
